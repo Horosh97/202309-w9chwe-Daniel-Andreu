6d0a9e4a7285819ae4a2b881b03910d2d62c3b05
Daniel Andreu <daniel1997ar@gmail.com>
2023-11-10 18:44:45 +0100

    Add HomePage.tsx and route to it in App.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,11 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "../../pages/HomePage/HomePage";
 
 const App = (): React.ReactElement => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="/films" element={<HomePage />} />
     </Routes>
   );
 };
