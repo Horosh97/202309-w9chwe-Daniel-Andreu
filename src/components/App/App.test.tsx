cf2a54e942f44d63874fcd1d0d95b8b94b7cf285
Daniel Andreu <daniel1997ar@gmail.com>
2023-11-11 13:44:09 +0100

    Fix App.tsx and App.test.tsx

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,23 +1,15 @@
-import { render, screen } from "@testing-library/react";
-import GlobalStyle from "../../styles/GlobalStyle";
-import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
-import { mainTheme } from "../../styles/mainTheme";
+import { screen } from "@testing-library/react";
 import App from "./App";
+import renderWithProviders from "../../testUtils/renderWithProviders";
+import usersMock from "../../mocks/usersMock";
 
 describe("Given a App component", () => {
   describe("When it renders", () => {
-    test("It should create a Header component with a heading", () => {
-      render(
-        <ThemeProvider theme={mainTheme}>
-          <BrowserRouter>
-            <GlobalStyle />
-            <App />
-          </BrowserRouter>
-        </ThemeProvider>,
-      );
+    test("It should create a Header component with a heading 'ISDIgram'", () => {
+      const expectedHeading = "ISDIgram";
+      renderWithProviders(<App />, usersMock);
 
-      const Heading = screen.getByRole("heading");
+      const Heading = screen.getByRole("heading", { name: expectedHeading });
 
       expect(Heading).toBeInTheDocument();
     });
